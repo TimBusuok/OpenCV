2c5a965c60aeaa5d11027e3b4a7b7777245094e5
unknown <timone300003@gmail.com>
2023-06-16 15:21:12 +0300

    work with picture

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 
 img = cv2.imread("test.jpg")
-print(img.shape)
-img = cv2.resize(img, (500, 500))
-print(img.shape)
+#img = cv2.resize(img, (500, 500))
+print(img)
+
+cv2.imshow("Result", img)
+
+cv2.waitKey(0)
